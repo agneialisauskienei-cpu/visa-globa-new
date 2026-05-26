bf7b0ad7758735eb6f490a48b56e7f653aa282a0
Your Name <you@example.com>
2026-05-26 22:23:41 +0300

    Add invites module

--- app/(app)/team/TeamClient.tsx
+++ app/(app)/team/TeamClient.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Suspense, useEffect, useMemo, useState } from "react";
-import InvitesModule from "./components/Invites/InvitesModule"
+import InvitesModule from "./components/Invites/InvitesModule";
 import {
   AlertTriangle,
   ArrowRight,
@@ -14,6 +14,7 @@ import {
   Edit3,
   FileText,
   GraduationCap,
+  Mail,
   PackageCheck,
   Plus,
   RefreshCw,
